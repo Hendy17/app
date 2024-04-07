cf7cd34ffdd7c5f54e88965cdb72d9f6d99ba8fc
durexex <eduardodurieuxlopes@gmail.com>
2024-04-06 21:38:45 -0300

    primeiro commit backend

--- Hercules/src/main/java/com/durex/Hercules/HerculesApplication.java
+++ Hercules/src/main/java/com/durex/Hercules/HerculesApplication.java
@@ -1,4 +1,4 @@
-package com.durex.Hercules;
+package com.durex.hercules;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Hercules/src/test/java/com/durex/Hercules/HerculesApplicationTests.java
+++ Hercules/src/test/java/com/durex/Hercules/HerculesApplicationTests.java
@@ -1,4 +1,4 @@
-package com.durex.Hercules;
+package com.durex.hercules;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
